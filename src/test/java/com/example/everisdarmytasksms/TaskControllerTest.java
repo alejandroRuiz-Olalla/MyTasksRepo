45237a4514ce139e69f13f97431a8accb00888c0
alejandroRuiz-Olalla <75794301+alejandroRuiz-Olalla@users.noreply.github.com>
2020-12-17 11:24:12 +0100

    Arreglados los test2

--- src/test/java/com/example/everisdarmytasksms/TaskControllerTest.java
+++ src/test/java/com/example/everisdarmytasksms/TaskControllerTest.java
@@ -1,6 +1,6 @@
 package com.example.everisdarmytasksms;
 
-import static org.junit.jupiter.api.Assertions.*;
+
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -15,11 +15,8 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
 
 import com.example.everisdarmytasksms.controller.TasksController;
 import com.example.everisdarmytasksms.modelo.Task;
@@ -37,32 +34,51 @@ class TaskControllerTest {
 	@Test
 	public void Tasks_All() throws Exception{
 		when(tasksService.findAll()).thenReturn(
-				Arrays.asList(new Task((long)2,"task","Ya esta hecho","Completada"),
-						new Task((long)3,"Tercera tarea y ultima de prueba","Acabar de diseñar todo","Pendiente"),
-						new Task((long)4,"Segunda tarea","","Completada"))
+				Arrays.asList(new Task((long)1,"Primera tarea","Descripcion de prueba","Pendiente"),
+						new Task((long)2,"Segunda tarea","","Completada"),
+						new Task((long)4,"task5","task5","Pendiente"))
 				);
 		RequestBuilder request = MockMvcRequestBuilders
 				.get("/tasks")
 				.accept(MediaType.APPLICATION_JSON);
 		
-		MvcResult result = mockMvc.perform(request)
+		/*MvcResult result = */mockMvc.perform(request)
 				.andExpect(status().isOk())
-				.andExpect(content().json("[{'id':2,'firt_name':'task','task_description':'Ya esta hecho','estado':'Completada'},{'id':3,'firt_name':'Tercera tarea y ultima de prueba','task_description':'Acabar de diseñar todo','estado':'Pendiente'},{'id':4,'firt_name':'Segunda tarea','task_description':'','estado':'Completada'}]"))
+				.andExpect(content().json("[\r\n" + 
+						"    {\r\n" + 
+						"        \"id\": 1,\r\n" + 
+						"        \"first_name\": \"Primera tarea\",\r\n" + 
+						"        \"task_description\": \"Descripcion de prueba\",\r\n" + 
+						"        \"estado\": \"Pendiente\"\r\n" + 
+						"    },\r\n" + 
+						"    {\r\n" + 
+						"        \"id\": 2,\r\n" + 
+						"        \"first_name\": \"Segunda tarea\",\r\n" + 
+						"        \"task_description\": \"\",\r\n" + 
+						"        \"estado\": \"Completada\"\r\n" + 
+						"    },\r\n" + 
+						"    {\r\n" + 
+						"        \"id\": 4,\r\n" + 
+						"        \"first_name\": \"task5\",\r\n" + 
+						"        \"task_description\": \"task5\",\r\n" + 
+						"        \"estado\": \"Pendiente\"\r\n" + 
+						"    }\r\n" + 
+						"]"))
 				.andReturn();
 	}
 	
 	@Test
 	public void Tasks_Id() throws Exception{
-		Task esperado = new Task((long)2,"task","Ya esta hecho","Completada");
+		Task esperado = new Task((long)2,"Segunda tarea","","Completada");
 		Optional<Task> task = Optional.of(esperado);
 		when(tasksService.findById((long)2)).thenReturn(task);
 		RequestBuilder request = MockMvcRequestBuilders
-				.get("/tasks/buscarPorID/2")
+				.get("/task/2")
 				.accept(MediaType.APPLICATION_JSON);
 		
-		MvcResult result = mockMvc.perform(request)
+		/*MvcResult result = */mockMvc.perform(request)
 				.andExpect(status().isOk())
-				.andExpect(content().json("{'id':2,'firt_name':'task','task_description':'Ya esta hecho','estado':'Completada'}"))
+				.andExpect(content().json("{'id':2,'first_name':'Segunda tarea','task_description':'','estado':'Completada'}"))
 				.andReturn();
 	}
 	
@@ -71,43 +87,29 @@ class TaskControllerTest {
 		Task esperado = new Task((long)6,"Quinta tarea","jklñasdfjklñjklñasdf","Pendiente");
 		when(tasksService.saveByEntity(Mockito.any(Task.class))).thenReturn(esperado);
 		RequestBuilder request = MockMvcRequestBuilders
-				.post("http://localhost:8080/tasks/insert")
-				.content("{\"id\":6,\"firt_name\":\"Quinta tarea\",\"task_description\":\"jklñasdfjklñjklñasdf\",\"estado\":\"Pendiente\"}")
+				.post("http://localhost:8080/task")
+				.content("{\"id\":6,\"first_name\":\"Quinta tarea\",\"task_description\":\"jklñasdfjklñjklñasdf\",\"estado\":\"Pendiente\"}")
 				.contentType(MediaType.APPLICATION_JSON)
 				.accept(MediaType.APPLICATION_JSON);
 		
-		MvcResult result = mockMvc.perform(request)
+		/*MvcResult result = */mockMvc.perform(request)
 				.andExpect(status().isOk())
-				.andExpect(content().json("{'id':6,'firt_name':'Quinta tarea','task_description':'jklñasdfjklñjklñasdf','estado':'Pendiente'}"))
+				.andExpect(content().json("{'id':6,'first_name':'Quinta tarea','task_description':'jklñasdfjklñjklñasdf','estado':'Pendiente'}"))
 				.andReturn();
 	}
 	
 	@Test
 	public void Tasks_Delete() throws Exception{
-		Task esperado = new Task((long)2,"task","Ya esta hecho","Completada");
+		Task esperado = new Task((long)2,"Segunda tarea","","Completada");
 		Optional<Task> task = Optional.of(esperado);
-		when(tasksService.findById((long)2)).thenReturn(task);
+		when(tasksService.deleteById((long)2)).thenReturn(task);
 		RequestBuilder request = MockMvcRequestBuilders
-				.delete("/tasks/2")
+				.delete("/task/2")
 				.accept(MediaType.APPLICATION_JSON);
 		
-		MvcResult result = mockMvc.perform(request)
+		/*MvcResult result = */mockMvc.perform(request)
 				.andExpect(status().isOk())
-				.andExpect(content().json("{'id':2,'firt_name':'task','task_description':'Ya esta hecho','estado':'Completada'}"))
+				.andExpect(content().json("{'id':2,'first_name':'Segunda tarea','task_description':'','estado':'Completada'}"))
 				.andReturn();
 	}
-	
-//	@Test
-//	public void Tasks_() throws Exception{
-//		RequestBuilder request = MockMvcRequestBuilders
-//				.get("/tasks")
-//				.accept(MediaType.APPLICATION_JSON);
-//		
-//		MvcResult result = mockMvc.perform(request)
-//				.andExpect(status().isOk())
-//				.andExpect(content().string("Hello World"))
-//				.andReturn();
-//		fail("Not yet implemented");
-//	}
-
 }
